b02ea8f65a6c5de189581b58ebcbec9df4d01df5
Dmytro <dm.kalitventsev@gmail.com>
2023-04-27 15:23:05 +0300

    changed the image search by click

--- js/script.js
+++ js/script.js
@@ -15,11 +15,15 @@ class Gallery {
 
 			obj.url = elem.getAttribute('src');
 			obj.title = elem.dataset.title;
-			obj.descr = elem.dataset.descr;
+			obj.descr = elem.dataset.description;
 
 			temp.push(obj);
 		});
 		return temp;
+	}
+
+	get overlay() {
+		return
 	}
 
 	generateModalWindow() {
@@ -44,53 +48,60 @@ class Gallery {
 	generateStyle() {
 		let str = `
 				<style>
-				.overlay {
-					position: fixed;
-					top: 0;
-					left: 0;
-					right: 0;
-					bottom: 0;
-					background-color: rgba(0, 0, 0, 0.8);
-					display: flex;
-					justify-content: center;
-					align-items: center;
-				}
-				
-				.modal {
-					width: 960px;
-					height: 427px;
-					overflow: hidden;
-					background-color: #fff;
-					box-shadow: 0 0 10px #000;
-					display: flex;
-				}
-				
-				.modal img {
-					width: 630px;
-				}
-				
-				.text {
-					padding: 44px;
-					display: flex;
-					flex-direction: column;
-					justify-content: space-between;
-				}
-				
-				.none {
-					display: none;
-				}
+					.overlay {
+						position: fixed;
+						top: 0;
+						left: 0;
+						right: 0;
+						bottom: 0;
+						background-color: rgba(0, 0, 0, 0.8);
+						display: flex;
+						justify-content: center;
+						align-items: center;
+					}
+					
+					.modal {
+						width: 960px;
+						height: 427px;
+						overflow: hidden;
+						background-color: #fff;
+						box-shadow: 0 0 10px #000;
+						display: flex;
+					}
+					
+					.modal img {
+						width: 630px;
+					}
+					
+					.text {
+						padding: 44px;
+						display: flex;
+						flex-direction: column;
+						justify-content: space-between;
+					}
+					
+					.none {
+						display: none;
+					}
 				</style>`;
 
 		document.head.insertAdjacentHTML('beforeend', str)
 	}
 
 	openModalWindow(even) {
-		let target = even.target;
+		const targetPic = even.target.closest('img');
 
-		if(target.matches('img')) {
-			let t = target.getAttribute('src');
+		if (targetPic) {
+			this.currentPic = this.allPicture.findIndex(picture => picture.url === targetPic.getAttribute('src'));
 
-			document.querySelector('.modal img').setAttribute('src', t);
+			const scr = this.allPicture[this.currentPic].url;
+			const title = this.allPicture[this.currentPic].title;
+			const descr = this.allPicture[this.currentPic].descr;
+
+			document.querySelector('.modal img').setAttribute('src', scr);
+			document.querySelector('.modal h2').innerText = title;
+			document.querySelector('.modal p').innerText = descr;
+
 			document.querySelector('.overlay').classList.remove('none');
 		}
 	}
@@ -101,13 +112,19 @@ class Gallery {
 		}
 	}
 
+	switchContent() {
+
+	}
+
 	init() {
 		console.dir(this);
+		console.log(this.allPicture);
 		this.generateModalWindow();
 		this.generateStyle();
 		this.gallery.classList.add('gallery');
 		this.gallery.addEventListener('click', this.openModalWindow.bind(this));
 		document.body.addEventListener('click', this.closeModalWindow.bind(this));
+
 	}
 }
 
