8d6b1a0e050033c99af0d95d07df7cd3aaaa9221
Dmytro <dm.kalitventsev@gmail.com>
2023-04-30 22:08:02 +0300

    redid the logic in JS for new styles and classes

--- js/script.js
+++ js/script.js
@@ -21,20 +21,20 @@ class Gallery {
 		return temp;
 	}
 
-	get modalWindow() {
-		return document.querySelector('.modal');
-	}
-
 	get overlay() {
 		return document.querySelector('.overlay');
 	}
 
-	get modalBtns() {
-		return this.modalWindow.querySelector('.btn');
+	get modalWindow() {
+		return document.querySelector('.modal-gallery');
 	}
 
-	get closeBtn() {
-		return this.modalWindow.querySelector('.close');
+	get modalToggleBtns() {
+		return this.modalWindow.querySelector('.modal-gallery__toggle-btns');
+	}
+
+	get modalCloseBtn() {
+		return this.modalWindow.querySelector('.modal-gallery__close');
 	}
 
 	get countPic() {
@@ -43,133 +43,138 @@ class Gallery {
 	
 	generateModalWindow() {
 		let str = `
-				<div class="overlay">
-					<div class="modal">
-						<img src="img/pic1.jfif" alt="img">
-						<div class="text">
-							<h2>Lorem ipsum</h2>
-							<p>
-								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Egestas fermentum pellentesque gravida in nisl faucibus etiam. Porta viverra nibh morbi tellus donec mattis porttitor fusce.
-							</p>
-							<div class="btn">
-								<button class="btn__prev">&lt;</button>
-								<button class="btn__next">&gt;</button>
-							</div>
+				<div class="overlay hidden">
+					<div class="modal-gallery">
+						<div class="modal-gallery__pic-wrapper">
+							<img class="modal-gallery__pic" src="" alt="pic">
 						</div>
-						<div class="close">
-							<span class="close__item"></span>
+						<div class="modal-gallery__info">
+							<div class="modal-gallery__close">
+								<img class="modal-gallery__close-img" src="img/icon/close.svg" alt="close">
+							</div>
+							<div class="modal-gallery__text">
+								<h2 class="modal-gallery__title"></h2>
+								<div class="modal-gallery__decor">
+									<svg class="modal-gallery__icon-decor" width="101" height="9" viewBox="0 0 101 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+										<rect width="101" height="9" />
+									</svg>
+									<svg class="modal-gallery__icon-decor" width="371" height="9" viewBox="0 0 371 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+										<rect width="371" height="9" />
+									</svg>
+									<svg class="modal-gallery__icon-decor" width="8" height="9" viewBox="0 0 8 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+										<rect width="8" height="9" transform="matrix(-1 0 0 1 8 0)" />
+									</svg>
+								</div>
+								<p class="modal-gallery__descr"></p>
+							</div>
+							<div class="modal-gallery__toggle-btns">
+								<div class="modal-gallery__toggle-btn">
+									<img class="modal-gallery__toggle-img modal-gallery__toggle-img_prev" src="img/icon/arrow.svg" alt="prev">
+								</div>
+								<div class="modal-gallery__toggle-btn">
+									<img class="modal-gallery__toggle-img modal-gallery__toggle-img_next" src="img/icon/arrow.svg" alt="next">
+								</div>
+							</div>
 						</div>
 					</div>
 				</div>`;
 
-		document.body.insertAdjacentHTML('beforeend', str)
+		document.body.insertAdjacentHTML('beforeend', str);
 	}
 
 	generateStyle() {
 		let str = `
 				<style>
 					.overlay {
+						display: grid;
+						justify-items: center;
+						align-items: center;
 						position: fixed;
 						top: 0;
 						left: 0;
 						right: 0;
 						bottom: 0;
-						background-color: rgba(0, 0, 0, 0.8);
-						display: flex;
-						justify-content: center;
-						align-items: center;
+						padding: 0 15px;
+						background-color: rgba(0, 0, 0, 0.5);
 					}
-					
-					.modal {
+					.modal-gallery {
+						display: grid;
+						grid-template-columns: 2fr minmax(270px, 1fr);
 						position: relative;
-						width: 960px;
-						height: 427px;
+						max-width: 960px;
 						overflow: hidden;
 						background-color: #fff;
-						box-shadow: 0 0 10px #000;
-						display: flex;
+						box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
 					}
-					
-					.modal img {
-						width: 630px;
-						flex: 0 0 630px;
-					}
-					
-					.text {
-						padding: 44px;
-						display: flex;
-						flex-direction: column;
-						justify-content: space-around;
-					}
-
-					.text h2 {
-						font-size: 24px;
-						text-transform: uppercase;
-					}
-
-					.text p {
-						font-size: 18px;
-						line-height: 1.23;
-					}
-					
-					.hidden {
-						display: none;
-					}
-					
-					button {
-						padding: 20px;
-					}
-					
-					.btn {
+					.modal-gallery__pic-wrapper {
 						position: relative;
+						padding: 0px 0px 67% 0px;
+						overflow: hidden;
 					}
-					
-					.btn__prev, .btn__next {
+					.modal-gallery__pic {
 						position: absolute;
-						top: -20px;
+						top: 0;
+						left: 0;
+						object-fit: cover;
 					}
-					
-					.btn__prev {
-						right: 25%;
+					.modal-gallery__info {
+						display: grid;
+						grid-template-rows: 1fr auto;
+						padding: 45px;
 					}
-					
-					.btn__next {
-						right: 0%;
-					}
-
-					.close {
-						display: flex;
-						justify-content: center;
-						align-items: center;
-						flex-direction: column;
-						flex: 0 0 50px;
+					.modal-gallery__close {
 						position: absolute;
-						top: 5px;
-						right: 5px;
-						width: 40px;
-						height: 40px;
-						border-radius: 15px;
-						transition: background-color 0.3s ease 0s;
+						top: 13px;
+						right: 20px;
+						height: 15px;
 						cursor: pointer;
 					}
-					
-					.close__item,
-					.close::before {
-						content: "";
+					.modal-gallery__text {
+						display: grid;
+						grid-auto-flow: row;
+						grid-template-rows: auto 1fr;
+						position: relative;
+						max-width: 245px;
+					}
+					.modal-gallery__title {
+						font-size: 1.3333rem;
+						line-height: 1.16;
+						text-transform: uppercase;
+					}
+					.modal-gallery__decor {
+						display: grid;
+						align-items: center;
+						grid-template-columns: 1fr 4fr auto;
 						position: absolute;
-						width: 60%;
-						height: 2px;
-						border-radius: 20px;
-						background: #C4C4C4;
-						transition: transform 0.3s ease 0s;
+						top: 45px;
+						left: -145px;
 					}
-					
-					.close::before {
-						transform: rotate(45deg);
+					.modal-gallery__icon-decor {
+						color: #ffad00;
+						height: 9px;
+						&:last-child {
+							margin-left: 5px;
+						}
 					}
-					
-					.close__item {
-						transform: rotate(-45deg);
+					.modal-gallery__descr {
+						margin-top: 70px;
+						line-height: 1.23;
+						max-height: 350px;
+						overflow: hidden;
+					}
+					.modal-gallery__toggle-btns {
+						display: grid;
+						justify-content: end;
+						justify-items: end;
+						grid-template-columns: repeat(2, minmax(65px, auto));
+						margin-top: 40px;
+					}
+					.modal-gallery__toggle-btn {
+						height: 24px;
+						cursor: pointer;
+					}
+					.modal-gallery__toggle-img_prev {
+						transform: rotate(180deg);
 					}
 				</style>`;
 
@@ -189,7 +194,7 @@ class Gallery {
 	}
 
 	closeModalWindow(event) {
-		if (event.target.matches('.overlay') || event.target.matches('.close')) {
+		if (event.target.matches('.overlay') || event.target.matches('.modal-gallery__close-img')) {
 			event.stopPropagation();
 
 			this.overlay.classList.add('hidden');
@@ -204,9 +209,9 @@ class Gallery {
 		const title = this.allPicture[indexPic].title;
 		const descr = this.allPicture[indexPic].descr;
 
-		this.modalWindow.querySelector('img').setAttribute('src', url);
-		this.modalWindow.querySelector('h2').innerText = title;
-		this.modalWindow.querySelector('p').innerText = descr;
+		this.modalWindow.querySelector('.modal-gallery__pic').setAttribute('src', url);
+		this.modalWindow.querySelector('.modal-gallery__title').innerText = title;
+		this.modalWindow.querySelector('.modal-gallery__descr').innerText = descr;
 
 		this.#currentPic = indexPic;
 
@@ -214,34 +219,34 @@ class Gallery {
 	}
 
 	showModalBtns(indexPic) {
-		const prevBtn = this.modalWindow.querySelector('.btn__prev');
-		const nextBtn = this.modalWindow.querySelector('.btn__next');
+		const prevBtn = this.modalWindow.querySelector('.modal-gallery__toggle-img_prev');
+		const nextBtn = this.modalWindow.querySelector('.modal-gallery__toggle-img_next');
 
 		(indexPic === 0) ? prevBtn.classList.add('hidden') : prevBtn.classList.remove('hidden');
 		(indexPic === this.countPic) ? nextBtn.classList.add('hidden') : nextBtn.classList.remove('hidden');
 	}
 
 	changeContent(event) {
-		if (event.target.matches('.btn__prev')) {
+		if (event.target.matches('.modal-gallery__toggle-img_prev')) {
 			this.fillContent(this.#currentPic - 1);
 		}
 
-		if (event.target.matches('.btn__next')) {
+		if (event.target.matches('.modal-gallery__toggle-img_next')) {
 			this.fillContent(this.#currentPic + 1);
 		}
 	}
 
 	init() {
-		// this.generateModalWindow();
-		// this.generateStyle();
+		this.generateModalWindow();
+		this.generateStyle();
 		this.overlay;
 		this.modalWindow;
 
 		this.gallery.classList.add('hover-effect');
 		this.gallery.addEventListener('click', this.openModalWindow.bind(this));
-		this.closeBtn.addEventListener('click', this.closeModalWindow.bind(this));
 		this.overlay.addEventListener('click', this.closeModalWindow.bind(this));
-		this.modalBtns.addEventListener('click', this.changeContent.bind(this));
+		this.modalCloseBtn.addEventListener('click', this.closeModalWindow.bind(this));
+		this.modalToggleBtns.addEventListener('click', this.changeContent.bind(this));
 	}
 }
 
