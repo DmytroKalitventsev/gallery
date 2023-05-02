46a744ba22bc5c958ca0c1c0c18f4802f7fa89eb
Dmytro <dm.kalitventsev@gmail.com>
2023-05-02 19:19:58 +0300

    made responsive for the gallery

--- js/script.js
+++ js/script.js
@@ -43,10 +43,10 @@ class Gallery {
 
 	generateModalWindow() {
 		let str = `
-				<div class="overlay" style="display: none">
+				<div class="overlay" style="display: none;">
 					<div class="modal-gallery">
 						<div class="modal-gallery__pic-wrapper">
-							<img class="modal-gallery__pic" src="" alt="pic">
+							<img class="modal-gallery__pic" src="" alt="picture">
 						</div>
 						<div class="modal-gallery__info">
 							<div class="modal-gallery__close">
@@ -55,24 +55,20 @@ class Gallery {
 							<div class="modal-gallery__text">
 								<h2 class="modal-gallery__title"></h2>
 								<div class="modal-gallery__decor">
-									<svg class="modal-gallery__icon-decor" width="101" height="9" viewBox="0 0 101 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<rect width="101" height="9" />
-									</svg>
-									<svg class="modal-gallery__icon-decor" width="371" height="9" viewBox="0 0 371 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<rect width="371" height="9" />
-									</svg>
-									<svg class="modal-gallery__icon-decor" width="8" height="9" viewBox="0 0 8 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-										<rect width="8" height="9" transform="matrix(-1 0 0 1 8 0)" />
-									</svg>
+									<span class="modal-gallery__elem-decor"></span>
+									<span class="modal-gallery__elem-decor"></span>
+									<span class="modal-gallery__elem-decor"></span>
 								</div>
 								<p class="modal-gallery__descr"></p>
 							</div>
 							<div class="modal-gallery__toggle-btns">
 								<div class="modal-gallery__toggle-btn">
-									<img class="modal-gallery__toggle-img modal-gallery__toggle-img_prev" src="img/icon/arrow.svg" alt="prev">
+									<img class="modal-gallery__toggle-img modal-gallery__toggle-img_prev" src="img/icon/arrow.svg"
+										alt="prev">
 								</div>
 								<div class="modal-gallery__toggle-btn">
-									<img class="modal-gallery__toggle-img modal-gallery__toggle-img_next" src="img/icon/arrow.svg" alt="next">
+									<img class="modal-gallery__toggle-img modal-gallery__toggle-img_next" src="img/icon/arrow.svg"
+										alt="next">
 								</div>
 							</div>
 						</div>
@@ -108,11 +104,22 @@ class Gallery {
 						overflow: hidden;
 						background-color: #fff;
 						box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+					
+						@media (max-width: 600px) {
+							grid-template: auto auto / auto;
+						}
 					}
 					.modal-gallery__pic-wrapper {
 						position: relative;
 						padding: 0px 0px 67% 0px;
 						overflow: hidden;
+					
+						@media (max-width: 600px) {
+							order: 2;
+						}
+						@media (max-width: 430px) {
+							padding: 0px 0px 100% 0px;
+						}
 					}
 					.modal-gallery__pic {
 						position: absolute;
@@ -124,6 +131,13 @@ class Gallery {
 						display: grid;
 						grid-template-rows: 1fr auto;
 						padding: 45px;
+					
+						@media (max-width: 767.97px) {
+							padding: 30px;
+						}
+						@media (max-width: 600px) {
+							order: 1;
+						}
 					}
 					.modal-gallery__close {
 						position: absolute;
@@ -133,37 +147,64 @@ class Gallery {
 						cursor: pointer;
 					}
 					.modal-gallery__text {
-						display: grid;
-						grid-auto-flow: row;
-						grid-template-rows: auto 1fr;
 						position: relative;
-						max-width: 245px;
 					}
 					.modal-gallery__title {
 						font-size: 1.3333rem;
 						line-height: 1.16;
 						text-transform: uppercase;
+						max-height: 30px;
+						overflow: hidden;
+					
+						@media (max-width: 767.97px) {
+							font-size: 1.1111rem;
+							max-height: 23px;
+						}
 					}
 					.modal-gallery__decor {
 						display: grid;
 						align-items: center;
-						grid-template-columns: 1fr 4fr auto;
+						grid-template-columns: minmax(100px, 1fr) 3fr auto;
 						position: absolute;
 						top: 45px;
-						left: -145px;
-					}
-					.modal-gallery__icon-decor {
-						color: #ffad00;
-						height: 9px;
-						&:last-child {
-							margin-left: 5px;
+						right: 0px;
+						width: calc(145px + 100%);
+					
+						@media (max-width: 767.97px) {
+							top: 35px;
+							width: calc(130px + 100%);
 						}
+						@media (max-width: 600px) {
+							top: 30px;
+							right: 45px;
+						}
+					}
+					.modal-gallery__elem-decor {
+						background-color: #ffad00;
+						height: 9px;
+					}
+					.modal-gallery__elem-decor:last-child {
+						width: 8px;
+						margin-left: 5px;
+					}
+					.color-elem .modal-gallery__elem-decor:first-child {
+						background-color: #fff;
 					}
 					.modal-gallery__descr {
 						margin-top: 70px;
 						line-height: 1.23;
-						max-height: 350px;
+						max-height: 355px;
 						overflow: hidden;
+					
+						@media (max-width: 767.97px) {
+							font-size: 0.8889rem;
+							max-height: 235px;
+							margin-top: 50px;
+						}
+						@media (max-width: 600px) {
+							max-height: 140px;
+							margin-top: 40px;
+						}
 					}
 					.modal-gallery__toggle-btns {
 						display: grid;
@@ -171,6 +212,10 @@ class Gallery {
 						justify-items: end;
 						grid-template-columns: repeat(2, minmax(65px, auto));
 						margin-top: 40px;
+					
+						@media (max-width: 767.97px) {
+							margin-top: 30px;
+						}
 					}
 					.modal-gallery__toggle-btn {
 						height: 24px;
@@ -179,6 +224,8 @@ class Gallery {
 					.modal-gallery__toggle-img_prev {
 						transform: rotate(180deg);
 					}
+
+					${this.generateStyleAnimate()}
 				</style>`;
 
 		document.head.insertAdjacentHTML('beforeend', str)
@@ -241,7 +288,8 @@ class Gallery {
 		this.modalWindow.querySelector('.modal-gallery__pic').setAttribute('src', url);
 		this.modalWindow.querySelector('.modal-gallery__title').innerText = title;
 		this.modalWindow.querySelector('.modal-gallery__descr').innerText = descr;
-
+		(indexPic === 0) ? this.modalWindow.classList.add('color-elem') : this.modalWindow.classList.remove('color-elem');
+		
 		this.#currentPic = indexPic;
 
 		this.showModalBtns(indexPic);
@@ -258,24 +306,20 @@ class Gallery {
 	changeContent(event) {
 		if (event.target.matches('.modal-gallery__toggle-img_prev')) {
 			this.fillContent(this.#currentPic - 1);
-
-			this.modalWindow.classList.add('switching');
+			
 			this.modalWindow.classList.add('animate_in-left');
 
 			setTimeout(() => {
-				this.modalWindow.classList.remove('switching');
 				this.modalWindow.classList.remove('animate_in-left');
 			}, 500);
 		}
 
 		if (event.target.matches('.modal-gallery__toggle-img_next')) {
 			this.fillContent(this.#currentPic + 1);
-
-			this.modalWindow.classList.add('switching');
+			
 			this.modalWindow.classList.add('animate_in-right');
 
 			setTimeout(() => {
-				this.modalWindow.classList.remove('switching');
 				this.modalWindow.classList.remove('animate_in-right');
 			}, 500);
 		}
